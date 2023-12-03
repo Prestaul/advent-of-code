c4393880e5804390b88dc1d5ea0014bd66ea718a
Matt Dunlap <mattdunlap@uber.com>
2023-12-02 23:43:11 -0700

    Day 3 rewrite

--- 2023/day-3-part-1.js
+++ 2023/day-3-part-1.js
@@ -24,41 +24,42 @@ function main(input) {
     ).reduce((sum, n) => sum + Number(n.val), 0);
 }
 
-function firstSolution(input) {
-  const grid = input.split('\n').map(s => `.${s}.`.split(''));
-  const h = grid.length + 1;
-  const w = grid[0].length + 1;
-  const g = [new Array(w).fill('.'), ...grid, new Array(w).fill('.')];
+// ORIGINAL SOLUTION THAT I HATED
+// function main(input) {
+//   const grid = input.split('\n').map(s => `.${s}.`.split(''));
+//   const h = grid.length + 1;
+//   const w = grid[0].length + 1;
+//   const g = [new Array(w).fill('.'), ...grid, new Array(w).fill('.')];
 
-  let sum = 0;
-  let n = '';
-  let isPart = false;
-  for(let i = 1; i < h; i++) for(let j = 1; j < w; j++) {
-    if (isNaN(g[i][j])) continue;
+//   let sum = 0;
+//   let n = '';
+//   let isPart = false;
+//   for(let i = 1; i < h; i++) for(let j = 1; j < w; j++) {
+//     if (isNaN(g[i][j])) continue;
 
-    do {
-      n += g[i][j];
-      isPart ||= (g[i-1][j-1] !== '.'
-        || g[i-1][j] !== '.'
-        || g[i-1][j+1] !== '.'
-        || (g[i][j-1] !== '.' && isNaN(g[i][j-1]))
-        || (g[i][j+1] !== '.' && isNaN(g[i][j+1]))
-        || g[i+1][j-1] !== '.'
-        || g[i+1][j] !== '.'
-        || g[i+1][j+1] !== '.'
-      )
-    } while(!isNaN(g[i][++j]))
+//     do {
+//       n += g[i][j];
+//       isPart ||= (g[i-1][j-1] !== '.'
+//         || g[i-1][j] !== '.'
+//         || g[i-1][j+1] !== '.'
+//         || (g[i][j-1] !== '.' && isNaN(g[i][j-1]))
+//         || (g[i][j+1] !== '.' && isNaN(g[i][j+1]))
+//         || g[i+1][j-1] !== '.'
+//         || g[i+1][j] !== '.'
+//         || g[i+1][j+1] !== '.'
+//       )
+//     } while(!isNaN(g[i][++j]))
 
-    if(isPart) {
-        sum += Number(n);
-    }
+//     if(isPart) {
+//         sum += Number(n);
+//     }
 
-    n = '';
-    isPart = false;
-  }
+//     n = '';
+//     isPart = false;
+//   }
 
-  return sum;
-}
+//   return sum;
+// }
 
 exec(main, '2023/day-3-input');
 
--- 2023/day-3-part-2.js
+++ 2023/day-3-part-2.js
@@ -19,68 +19,68 @@ function main(input) {
   const numbers = getMatches(/\d+/g, input, input.indexOf('\n') + 1);
   const symbols = getMatches(/\*/g, input, input.indexOf('\n') + 1);
 
-  return symbols
-    .map(({ row: r, col: c }) => numbers.filter(({ row, col, len }) => Math.abs(row - r) <= 1 && (c >= col - 1 && c <= col + len)))
+  return symbols.map(({ row: r, col: c }) => numbers.filter(({ row, col, len }) => Math.abs(row - r) <= 1 && (c >= col - 1 && c <= col + len)))
     .map(nums => nums.length === 2 ? Number(nums[0].val) * Number(nums[1].val) : 0)
     .reduce((sum, n) => sum + n, 0);
 }
 
-function firstSolution(input) {
-  const lines = input.split('\n');
-  const grid = lines.map(s => `.${s}.`.split(''));
-  const h = grid.length + 1;
-  const w = grid[0].length + 1;
-  const g = [new Array(w).fill('.'), ...grid, new Array(w).fill('.')];
+// ORIGINAL SOLUTION THAT I HATED
+// function main(input) {
+//   const lines = input.split('\n');
+//   const grid = lines.map(s => `.${s}.`.split(''));
+//   const h = grid.length + 1;
+//   const w = grid[0].length + 1;
+//   const g = [new Array(w).fill('.'), ...grid, new Array(w).fill('.')];
 
-  function getNumContaining(i, j) {
-    if (isNaN(g[i][j])) return null;
+//   function getNumContaining(i, j) {
+//     if (isNaN(g[i][j])) return null;
 
-    const r = /\d+/g;
-    r.lastIndex = lines[i - 1].lastIndexOf('.', j - 1);
-    nums.push(r.exec(lines[i - 1])[0]);
-  }
+//     const r = /\d+/g;
+//     r.lastIndex = lines[i - 1].lastIndexOf('.', j - 1);
+//     nums.push(r.exec(lines[i - 1])[0]);
+//   }
 
-  function getNumAfter(i, j) {
-    if (isNaN(g[i][j])) return null;
+//   function getNumAfter(i, j) {
+//     if (isNaN(g[i][j])) return null;
 
-    const r = /\d+/g;
-    r.lastIndex = j - 1;
-    nums.push(r.exec(lines[i - 1])[0]);
-  }
+//     const r = /\d+/g;
+//     r.lastIndex = j - 1;
+//     nums.push(r.exec(lines[i - 1])[0]);
+//   }
 
-  let sum = 0;
-  let nums = [];
-  for(let i = 1; i < h; i++) for(let j = 1; j < w; j++) {
-    if (g[i][j] !== '*') continue;
+//   let sum = 0;
+//   let nums = [];
+//   for(let i = 1; i < h; i++) for(let j = 1; j < w; j++) {
+//     if (g[i][j] !== '*') continue;
 
-    getNumContaining(i, j - 1),
-    getNumAfter(i, j + 1)
-    if (g[i-1][j] === '.') {
-      getNumContaining(i - 1, j - 1);
-      getNumAfter(i - 1, j + 1);
-    } else {
-      getNumContaining(i - 1, j);
-    }
-    if (g[i + 1][j] === '.') {
-      console.log('A')
-      getNumContaining(i + 1, j - 1);
-      getNumAfter(i + 1, j + 1);
-    } else {
-      console.log('B', i+1, j)
-      getNumContaining(i + 1, j);
-    }
+//     getNumContaining(i, j - 1),
+//     getNumAfter(i, j + 1)
+//     if (g[i-1][j] === '.') {
+//       getNumContaining(i - 1, j - 1);
+//       getNumAfter(i - 1, j + 1);
+//     } else {
+//       getNumContaining(i - 1, j);
+//     }
+//     if (g[i + 1][j] === '.') {
+//       console.log('A')
+//       getNumContaining(i + 1, j - 1);
+//       getNumAfter(i + 1, j + 1);
+//     } else {
+//       console.log('B', i+1, j)
+//       getNumContaining(i + 1, j);
+//     }
 
-    nums = nums.map(Number).filter(n => !isNaN(n));
+//     nums = nums.map(Number).filter(n => !isNaN(n));
 
-    if (nums.length === 2) {
-      sum += nums[0] * nums[1];
-    }
+//     if (nums.length === 2) {
+//       sum += nums[0] * nums[1];
+//     }
 
-    nums.length = 0;
-  }
+//     nums.length = 0;
+//   }
 
-  return sum;
-}
+//   return sum;
+// }
 
 exec(main, '2023/day-3-input');
 
