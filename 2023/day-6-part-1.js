fbdc247bfc21b365ff9814bf825b71bf3a07bea3
Matt Dunlap <mattdunlap@uber.com>
2023-12-05 22:27:44 -0700

    Day 6

---
+++ 2023/day-6-part-1.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import { exec } from '../helpers/exec.js';
+
+function main(input) {
+  const [l1, l2] = input.trim().split('\n');
+  const times = l1.match(/\d+/g).map(Number);
+  const distances = l2.match(/\d+/g).map(Number);
+
+  return times.map((time, i) => {
+    let wins = 0;
+    const dist = distances[i];
+    for (let t = 1; t < time; t++) {
+      if (t * (time - t) > dist) wins += 1;
+    }
+
+    return wins;
+  }).reduce((m, n) => m * n);
+}
+
+// exec(main, '2023/day-2-input');
+
+console.log(main(`Time:        51     92     68     90
+Distance:   222   2031   1126   1225`));
+// console.log(main(`Time:      7  15   30
+// Distance:  9  40  200`));
---
+++ 2023/day-6-part-2.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import { exec } from '../helpers/exec.js';
+
+function main(input) {
+  const [l1, l2] = input.trim().split('\n');
+  const [time] = l1.replaceAll(/\s+/g, '').match(/\d+/g).map(Number);
+  const [dist] = l2.replaceAll(/\s+/g, '').match(/\d+/g).map(Number);
+
+  let wins = 0;
+  for (let t = 1; t < time; t++) {
+    if (t * (time - t) > dist) wins += 1;
+  }
+
+  return wins;
+}
+
+// exec(main, '2023/day-2-input');
+
+console.log(main(`Time:        51     92     68     90
+Distance:   222   2031   1126   1225`));
+// console.log(main(`Time:      7  15   30
+// Distance:  9  40  200`));
