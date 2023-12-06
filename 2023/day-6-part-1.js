99b093977bf473d78670c0ddd0bb7e43a6360f5e
Matt Dunlap <mattdunlap@uber.com>
2023-12-05 22:38:57 -0700

    With timing

--- 2023/day-6-part-1.js
+++ 2023/day-6-part-1.js
@@ -17,7 +17,9 @@ function main(input) {
   }).reduce((m, n) => m * n);
 }
 
+console.time()
 console.log(main(`Time:        51     92     68     90
 Distance:   222   2031   1126   1225`));
 // console.log(main(`Time:      7  15   30
 // Distance:  9  40  200`));
+console.timeEnd();
