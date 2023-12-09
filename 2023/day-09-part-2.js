538101249fabcf54fc8d6f05e71371e9616185a4
Matt Dunlap <mattdunlap@uber.com>
2023-12-08 22:35:40 -0700

    Day 9

---
+++ 2023/day-09-part-2.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import { exec } from '../helpers/exec.js';
+
+function next(diffs, n, i, a) {
+  if (i + 1 < a.length) {
+    diffs.push(a[i + 1] - n);
+    return diffs;
+  }
+  if (a.every(v => !v)) return 0;
+  return a[0] - diffs.reduce(next, []);
+}
+
+function main(input) {
+  return input.split('\n').map(l => l.match(/-?\d+/g).map(Number).reduce(next, [])).reduce((a, b) => a + b);
+}
+
+exec(main, '2023/day-09-input'); // 925
+
+console.log(main(`0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`)); // 2
