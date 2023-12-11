0356069410d73b8ba1d2cf5e00b163be485fbd85
Matt Dunlap <mattdunlap@uber.com>
2023-12-10 23:32:20 -0700

    Day 11

---
+++ 2023/day-11-part-1.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+import { exec } from '../helpers/exec.js';
+
+function expand(input) {
+  const rows = input.replaceAll(/^\.+$/gm, x => `${x}\n${x}`).split('\n');
+
+  let i = rows[0].length;
+  column: while(i--) {
+    for (let r of rows) {
+      if (r[i] !== '.') continue column;
+    }
+    let j = rows.length;
+    while(j--) rows[j] = rows[j].substring(0, i) + '.' + rows[j].substring(i);
+  }
+
+  return rows.join('\n');
+}
+
+function dist(a, b) {
+  return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+}
+
+function main(input) {
+  const map = expand(input);
+  const w = map.indexOf('\n') + 1;
+  const galaxies = [];
+
+  let i = -1;
+  while(true) {
+    i = map.indexOf('#', i + 1);
+    if (i < 0) break;
+    galaxies.push({ x: i % w, y: Math.floor(i / w) });
+  }
+
+  let d = 0;
+  let a = galaxies.length;
+  while(a--) {
+    let b = a;
+    while(b--) d += dist(galaxies[a], galaxies[b]);
+  }
+
+  return d;
+}
+
+exec(main, '2023/day-11-input'); // 9734203
+
+console.log(main(`...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....`)); // 374
---
+++ 2023/day-11-part-2.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+import { exec } from '../helpers/exec.js';
+
+function main(input, expansion = 999999) {
+  const w = input.indexOf('\n') + 1;
+  const rows = input.split('\n');
+
+  const emptyRows = [];
+  let i = rows.length;
+  while(i--) if (/^\.+$/.test(rows[i])) emptyRows.push(i);
+
+  const emptyCols = [];
+  i = rows[0].length;
+  column: while(i--) {
+    for (let r of rows) {
+      if (r[i] !== '.') continue column;
+    }
+    emptyCols.push(i);
+  }
+
+  const galaxies = [];
+  i = -1;
+  while(true) {
+    i = input.indexOf('#', i + 1);
+    if (i < 0) break;
+    galaxies.push({ x: i % w, y: Math.floor(i / w) });
+  }
+
+  let d = 0;
+  let a = galaxies.length;
+  while(a--) {
+    const ga = galaxies[a];
+    let b = a;
+    while(b--) {
+      const gb = galaxies[b];
+      const maxX = Math.max(ga.x, gb.x);
+      const minX = Math.min(ga.x, gb.x);
+      const maxY = Math.max(ga.y, gb.y);
+      const minY = Math.min(ga.y, gb.y);
+      d += maxX - minX + maxY - minY + emptyRows.filter(y => y > minY && y < maxY).length * expansion + emptyCols.filter(x => x > minX && x < maxX).length * expansion;
+    };
+  }
+
+  return d;
+}
+
+exec(main, '2023/day-11-input'); // 568914596391
+
+console.log(main(`...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....`, 99)); // 8410
