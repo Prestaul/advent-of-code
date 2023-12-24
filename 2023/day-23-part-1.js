88517ffa7f866a02170bc5074e974d26cd19227a
Matt Dunlap <mattdunlap@uber.com>
2023-12-24 09:47:44 -0700

    Day 23 part 2

---
+++ 2023/day-23-part-1.js
@@ -0,0 +1,89 @@
+#!/usr/bin/env node
+import { exec, test } from '../helpers/exec.js';
+import { Heap } from 'heap-js';
+
+function walk(grid) {
+  const h = grid.length;
+  const w = grid[0].length;
+  const distances = Array(h).fill().map(_ => Array(w).fill(0));
+
+  const opposites = {
+    '^': 'v',
+    'v': '^',
+    '<': '>',
+    '>': '<',
+  };
+  const frontier = new Heap((a, b) => b[2] - a[2]);
+  frontier.init([[0, 1, 0, 'v']]);
+  let maxDist = 0;
+  function addToFrontier(r, c, dist, dir) {
+    const cell = grid[r]?.[c];
+    // console.log(r, c, cell, dir, opposites[dir], distances[r]?.[c])
+    if (!cell || cell === '#' || cell.includes(opposites[dir]) || distances[r][c] >= dist) return false;
+
+    distances[r][c] = dist;
+    frontier.push([r, c, dist, dir]);
+    return true;
+  }
+
+  let cell;
+  // let i = 0;
+  while (cell = frontier.pop()) {
+    const [r, c, dist, dir] = cell;
+    // console.log(cell);
+    if (addToFrontier(r - 1, c, dist + 1, '^')) grid[r][c] += '^';
+    if (addToFrontier(r + 1, c, dist + 1, 'v')) grid[r][c] += 'v';
+    if (addToFrontier(r, c - 1, dist + 1, '<')) grid[r][c] += '<';
+    if (addToFrontier(r, c + 1, dist + 1, '>')) grid[r][c] += '>';
+    // if (i++ > 1000) break;
+  }
+  // console.log(grid.map(s => s.map(s => s.at(-1)).join('')).join('\n'))
+  return distances[h - 1][w - 2];
+}
+
+const DIRS = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+function DFS(grid, r, c, dist, visited = Array(grid.length).fill().map(_ => Array(grid[0].length))) {
+  visited[r][c] = true;
+  let curLen = 0;
+
+  for (const [dr, dc] of DIRS) {
+    if (!visited[r + dr][c + dc]) {
+      DFS(gird, r + dr, c + dc, dist + 1, visited);
+    }
+  }
+
+  return visited;
+}
+
+function part1(input) {
+  return walk(input.split('\n').map(l => l.split('')));
+}
+
+const sampleInput = `
+#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#`.trim();
+test(part1, sampleInput, 94);
+
+const inputFile = '2023/day-23-input';
+exec(part1, inputFile); // => 2366
---
+++ 2023/day-23-part-2.js
@@ -0,0 +1,119 @@
+#!/usr/bin/env node
+import { exec, test } from '../helpers/exec.js';
+
+function getGraph(input) {
+  const grid = input.replaceAll(/[^#\n]/g, ' ').split('\n').map(l => l.split(''));
+  const h = grid.length - 1;
+  const graph = [];
+
+  function getNeighbors([r, c]) {
+    const cells = [];
+    if (grid[r - 1]?.[c] === ' ') cells.push([r - 1, c]);
+    if (grid[r + 1]?.[c] === ' ') cells.push([r + 1, c]);
+    if (grid[r][c - 1] === ' ') cells.push([r, c - 1]);
+    if (grid[r][c + 1] === ' ') cells.push([r, c + 1]);
+    return cells;
+  }
+
+  function edge(a, b, dist) {
+    graph[a].edges.push({ node: b, dist });
+    graph[b].edges.push({ node: a, dist });
+  }
+
+  function nextNode([r, c], from) {
+    if (grid[r][c] !== ' ') return;
+
+    let dist = 1;
+    while(true) {
+      if (typeof grid[r][c] === 'number') {
+        return edge(from, grid[r][c], dist);
+      }
+
+      grid[r][c] = '.';
+
+      const n = grid[r - 1]?.[c] === ' ' || (typeof grid[r - 1]?.[c] === 'number' && grid[r - 1]?.[c] !== from);
+      const s = grid[r + 1]?.[c] === ' ' || (typeof grid[r + 1]?.[c] === 'number' && grid[r + 1]?.[c] !== from);
+      const w = grid[r][c - 1] === ' ' || (typeof grid[r][c - 1] === 'number' && grid[r][c - 1] !== from);
+      const e = grid[r][c + 1] === ' ' || (typeof grid[r][c + 1] === 'number' && grid[r][c + 1] !== from);
+
+      if (r === h || n + s + e + w !== 1) {
+        return edge(from, walk(r, c), dist);
+      }
+
+      if (n) r -= 1;
+      if (s) r += 1;
+      if (w) c -= 1;
+      if (e) c += 1;
+
+      dist += 1;
+    }
+  }
+
+  function walk(r, c) {
+    const node = { r, c, edges: [] };
+    const index = graph.push(node) - 1;
+    grid[r][c] = index;
+    getNeighbors([r, c]).map(n => nextNode(n, index));
+    return index;
+  }
+
+  walk(0, 1);
+
+  // console.log(grid.map(s => s.map(n => typeof n === 'number' ? n.toString(36) : n).join('')).join('\n'))
+  // console.log(JSON.stringify(graph, null, 2));
+
+  return graph;
+}
+
+function DFS(graph, destination, { node: i = 0, dist = 0 } = {}, visited = new Set()) {
+  if (i === destination) return dist;
+
+  visited.add(i);
+
+  const maxDistance = Math.max(
+    ...graph[i].edges
+      .filter(({ node }) => !visited.has(node))
+      .map(edge => DFS(graph, destination, edge, visited))
+  );
+
+  visited.delete(i);
+
+  return dist + maxDistance;
+}
+
+function part2(input) {
+  const graph = getGraph(input);
+  const h = input.split('\n').length - 1;
+  return DFS(graph, graph.findIndex(({ r }) => r === h));
+}
+
+const sampleInput = `
+#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#`.trim();
+// test(part1, sampleInput, 94);
+test(part2, sampleInput, 154);
+
+const inputFile = '2023/day-23-input';
+// exec(part1, inputFile); // => 2366
+exec(part2, inputFile); // => 6682
