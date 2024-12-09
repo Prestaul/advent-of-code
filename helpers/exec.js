93f7fa434054ea8e6254b1389df52ffed9d982d7
Matt Dunlap <mpdunlap@gmail.com>
2024-12-09 13:01:30 -0700

    Update exec/test to only output 'expected' on failures

--- helpers/exec.js
+++ helpers/exec.js
@@ -18,13 +18,13 @@ export function exec(fn, inputFile, expected, ...args) {
   const input = readFileSync(join(ROOT, inputFile), { encoding: 'utf8' });
   console.log(' Running:', fn.name);
   const result = run(fn, input, ...args);
-  if (expected) console.log('Expected:', expected, result != expected ? '-\x1b[31m FAILED\x1b[0m' : '');
+  if (expected && result != expected) console.log('Expected:', expected, '-\x1b[31m FAILED\x1b[0m');
   console.log();
 }
 
 export function test(fn, input, expected, ...args) {
   console.log(' Testing:', fn.name);
   const result = run(fn, input, ...args);
-  console.log('Expected:', expected, result != expected ? '-\x1b[31m FAILED\x1b[0m' : '');
+  if (result != expected) console.log('Expected:', expected, '-\x1b[31m FAILED\x1b[0m');
   console.log();
 }
