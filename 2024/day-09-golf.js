1e33315892b7ec7031bdee850cac373844062bce
Matt Dunlap <mpdunlap@gmail.com>
2024-12-09 18:35:32 -0700

    Day 9 golf progress

--- 2024/day-09-golf.js
+++ 2024/day-09-golf.js
@@ -38,49 +38,64 @@ function part1Expanded(input, sum = 0) {
 function part2(input) {
   // Get list of files and empty space.
   let pos = 0;
-  const mem = [...input].map(Number).map((size, i) => {
-    const mem = { pos, size, id: i % 2 ? null : i / 2 };
+  const mem = [...input].map(s=>+s).map((size, id) => {
+    const mem = { pos, size, id: id % 2 ? null : id / 2 };
     pos += size;
     return mem;
   });
 
-  // Loop through files and insert them into empty spaces.
-  nextFile: for (let i = mem.length; i--;) {
-    const f = mem[i];
-    if (!f.id) continue; // Ignore empty spaces
-
-    // Find space large enough for file
-    for (let j = 1; j < i; j++) {
-      const s = mem[j];
-      if (s.id) continue; // Ignore files
-      if (s.size >= f.size) {
-        // Move the file and take memory from the empty space
-        f.pos = s.pos;
-        s.size -= f.size;
-        s.pos += f.size;
-        continue nextFile;
+  let sum = 0;
+  for (let r = mem.length - 1, file; file = mem[r], r >= 0; r -= 2) {
+    for (let l = 1, space; space = mem[l], l < r; l += 2)
+      if (file.size <= space.size) {
+        file.pos = space.pos;
+        space.size -= file.size;
+        space.pos += file.size;
+        break;
       }
-    }
+    while (file.size--) sum += file.id * file.pos++;
   }
 
-  return mem.reduce((sum, { id, size, pos }) => {
-    if (id) while (size--) sum += id * pos++;
-    return sum;
-  }, 0);
+  return sum;
 }
 
+function part2Expanded(input) {
+  // Get list of files and empty space.
+  let pos = 0;
+  const mem = [...input].map((size, id) => {
+    size=+size
+    const mem = { pos, size, id: id % 2 ? null : id / 2 };
+    pos += size;
+    return mem;
+  });
+
+  let sum = 0;
+  for (let r = mem.length - 1, file; file = mem[r], r >= 0; r -= 2) {
+    for (let l = 1, space; space = mem[l], l < r; l += 2)
+      if (file.size <= space.size) {
+        file.pos = space.pos;
+        space.size -= file.size;
+        space.pos += file.size;
+        break;
+      }
+    while (file.size--) sum += file.id * file.pos++;
+  }
+
+  return sum;
+}
 
 //  left pointer  v
 //         input  00...111...2...333.44.5555.6666.777.888899
 // right pointer                                           ^
 //        result  009981118882777333644655556
 
-const sampleInput = `
-2333133121414131402`.trim();
-test(part1, sampleInput, 1928);
-test(part2, sampleInput, 2858);
-
 const inputFile = 'inputs/2024/day-09.txt';
+const sampleInput = '2333133121414131402';
+
+test(part1, sampleInput, 1928);
 exec(part1, inputFile, 6430446922192);
 exec(part1Expanded, inputFile, 6430446922192);
+
+test(part2, sampleInput, 2858);
 exec(part2, inputFile, 6460170593016);
+exec(part2Expanded, inputFile, 6460170593016);
