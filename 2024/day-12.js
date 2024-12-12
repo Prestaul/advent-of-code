8acae82e007de42231b17611bddc6d677cba4b7f
Matt Dunlap <mpdunlap@gmail.com>
2024-12-12 12:01:33 -0700

    Day 12 cleanup

--- 2024/day-12.js
+++ 2024/day-12.js
@@ -43,11 +43,12 @@ function part1(input) {
   return sum;
 }
 
+// Order matters here
 const DIRS = [
-  [1, 0, 0, -1],
-  [0, 1, 1, 0],
-  [-1, 0, 0, 1],
-  [0, -1, -1, 0],
+  [ 1,  0],
+  [ 0,  1],
+  [-1,  0],
+  [ 0, -1],
 ];
 function part2(input) {
   const grid = input.split('\n').map(l => l.split(''));
@@ -71,9 +72,8 @@ function part2(input) {
         for (let d = 0; d < DIRS.length; d++) {
           const [dx1, dy1] = DIRS[d];
           const [dx2, dy2] = DIRS[(d + 1) % 4];
-          // Sum up the number of cells that match this cell on each diagonal.
-          //  d1 d2
-          //  d2 d1
+          // Count matches of this cell in the square on each diagonal.
+          // d1 = diagonal to us, d2 = the others
           const d1 = 1 + (grid[y + (dy1 || dy2)]?.[x + (dx1 || dx2)] === flower);
           const d2 = (grid[y][x + (dx1 || dx2)] === flower) + (grid[y + (dy1 || dy2)]?.[x] === flower);
           // Draw up all the possible 2x2 blocks, count up matches on diagonals, and you'll see that this condition finds corner cells.
@@ -89,219 +89,7 @@ function part2(input) {
   }
 
   return sum;
-  // return Object.values(plots).flat().reduce((sum, plot) => {
-  //   for (const [x, y] of plot) {
-  //     const flower = grid[y][x];
-  //     let sides =  0;
-  //     for (let d = 0; d < DIRS.length; d++) {
-  //       const [dx1, dy1] = DIRS[d];
-  //       const [dx2, dy2] = DIRS[(d + 1) % 4];
-  //       const d1 = 1 + (grid[y + (dy1 || dy2)]?.[x + (dx1 || dx2)] === flower);
-  //       const d2 = (grid[y][x + (dx1 || dx2)] === flower) + (grid[y + (dy1 || dy2)]?.[x] === flower);
-  //       if (d2 === 0 || (d1 === 1 && d2 === 2)) {
-  //         sides++;
-  //       }
-  //     }
-  //     // console.log({flower, plot, sides, cost: sides * plot.length})
-  //     return sum + sides * plot.length;
-  //   }
-  // }, 0);
-
-
-  // // console.log(Object.values(plots).flat());
-  // return Object.values(plots).flat().reduce((a, plot) => {
-  //   let sides = 1;
-  //   const [[ix, iy]] = plot;
-  //   const flower = grid[iy][ix];
-  //   // console.log({flower, plot});
-  //   let [[x, y]] = plot;
-
-  //   let d = 0;
-
-  //   let [dx, dy, wx, wy] = DIRS[d];
-  //   function turn(r)  {
-  //     sides++;
-  //     d = (d + 4 + r) % 4;
-  //     [dx, dy, wx, wy] = DIRS[d];
-  //   }
-
-  //   // console.log('corner', {d, x, y, /* dx, dy, wx, wy, */ sides, flower });
-  //   // Find the next corner
-  //   while(d !== 3 || x !== ix || y !== iy) {
-  //     if (grid[y + wy]?.[x + wx] === flower) {
-  //       turn(-1);
-  //       x += dx;
-  //       y += dy;
-  //       // console.log('turning left', {x, y, sides});
-  //     } else if (grid[y + dy]?.[x + dx] !== flower) {
-  //       turn(1);
-  //       // console.log('turning right', {x, y, sides});
-  //     } else if (grid[y + dy]?.[x + dx] === flower) {
-  //       x += dx;
-  //       y += dy;
-  //     }
-  //   }
-
-  //   console.log({flower, sides, area: plot.length });
-  //   return a + plot.length * sides;
-  // }, 0);
-
-  // return Object.values(plots).flat().reduce((a, { area, perimeter }) => a + area * perimeter, 0);
 }
-
-function part22(input) {
-  const grid = input.split('\n').map(l => l.split(''));
-  const h = grid.length;
-  const w = grid[0].length;
-  const visited = Array.from({ length: h }, () => new Array(w).fill(0));
-
-  const plots = {};
-  const frontier = [];
-  let sum = 0;
-  for (let x = 0; x < w; x++) for (let y = 0; y < h; y++) {
-    if (!visited[y][x]) {
-      const flower = grid[y][x];
-      const plot = { area: 0 };
-      (plots[flower] ??= []).push(plot);
-      // const walls = {
-      //   dx: { '-1': {}, 1: {}},
-      //   dy: { '-1': {}, 1: {}}
-      // };
-      const walls = {
-        dx: { '-1': new Set(), 1: new Set()},
-        dy: { '-1': new Set(), 1: new Set()}
-      };
-      // const walls = {
-      //   '-1,0': {},
-      //   '1,0': {},
-      //   '0,-1': {},
-      //   '0,1': {},
-      // };
-
-      frontier.push([x, y, 0, 0]);
-      while (frontier.length) {
-        let [x, y, dx, dy] = frontier.pop();
-        x += dx;
-        y += dy;
-
-        if (grid[y]?.[x] !== flower) {
-          // if (dx)
-          //   (walls.dx[dx][x] ??= []).push(y);
-          // else
-          //   (walls.dy[dy][y] ??= []).push(x);
-
-          // (walls[[dx,dy].join(',')][dx ? x : y] ??= new Set()).add(dx ? y : x);
-
-          if (dx)
-            walls.dx[dx].add([x, y].join(','));
-          else
-            walls.dy[dy].add([x, y].join(','));
-          continue;
-        }
-        if (visited[y]?.[x]) {
-          continue;
-        }
-
-        plot.area++;
-
-        visited[y][x] = 1;
-        frontier.push([x, y, 1, 0], [x, y, -1, 0], [x, y, 0, 1], [x, y, 0, -1]);
-      }
-
-      // console.log(flower, plot);
-      // console.log(walls);
-      if (plot.area < 3) {
-        sum += plot.area * 4;
-        continue;
-      }
-
-      const countSequences = (a, arr) => {
-        arr.sort();
-        for (let i = 1; i < arr.length; i++) if(arr[i] - arr[i - 1] > 1) a++;
-        return a + 1;
-      }
-
-      const c = (a, arr) => {
-        arr = [...arr].toSorted();
-        // console.log(arr);
-        for (let i = 1; i < arr.length; i++) if(arr[i] - arr[i - 1] > 1) a++;
-        return a + 1;
-      };
-      // console.log(walls);
-      // let count =
-      //   Object.values(walls.dx[-1]).reduce(c, 0);
-      //   Object.values(walls.dx[ 1]).reduce(c, 0);
-      //   Object.values(walls.dy[-1]).reduce(c, 0);
-      //   Object.values(walls.dy[ 1]).reduce(c, 0);
-        // Object.values([...walls.dx[-1]].map(s => s.split(',').map(Number)).reduce((a, [x, y]) => { (a[x] ??= []).push(y); return a; }, {})).reduce(countSequences, 0) +
-        // Object.values([...walls.dx[ 1]].map(s => s.split(',').map(Number)).reduce((a, [x, y]) => { (a[x] ??= []).push(y); return a; }, {})).reduce(countSequences, 0) +
-        // Object.values([...walls.dy[-1]].map(s => s.split(',').map(Number)).reduce((a, [x, y]) => { (a[y] ??= []).push(x); return a; }, {})).reduce(countSequences, 0) +
-        // Object.values([...walls.dy[ 1]].map(s => s.split(',').map(Number)).reduce((a, [x, y]) => { (a[y] ??= []).push(x); return a; }, {})).reduce(countSequences, 0);
-      let count =
-        Object.values([...walls.dx[-1]].map(s => s.split(',').map(Number)).reduce((a, [x, y]) => { (a[x] ??= []).push(y); return a; }, {})).reduce(countSequences, 0) +
-        Object.values([...walls.dx[ 1]].map(s => s.split(',').map(Number)).reduce((a, [x, y]) => { (a[x] ??= []).push(y); return a; }, {})).reduce(countSequences, 0) +
-        Object.values([...walls.dy[-1]].map(s => s.split(',').map(Number)).reduce((a, [x, y]) => { (a[y] ??= []).push(x); return a; }, {})).reduce(countSequences, 0) +
-        Object.values([...walls.dy[ 1]].map(s => s.split(',').map(Number)).reduce((a, [x, y]) => { (a[y] ??= []).push(x); return a; }, {})).reduce(countSequences, 0);
-
-      sum += plot.area * count;
-    }
-  }
-  return sum;
-}
-
-function part23(input) {
-  // Counts corners correctly but incorrectly combines plots with same letter
-  const grid = input.split('\n').map(l => l.split(''));
-  const h = grid.length;
-  const w = grid[0].length;
-  let sum = 0;
-  for (let x = 0; x < w; x++) for (let y = 0; y < h; y++) {
-    const flower = grid[y][x];
-    for (let d = 0; d < DIRS.length; d++) {
-      const [dx1, dy1] = DIRS[d];
-      const [dx2, dy2] = DIRS[(d + 1) % 4];
-
-      const d1 = 1 + (grid[y + (dy1 || dy2)]?.[x + (dx1 || dx2)] === flower);
-      const d2 = (grid[y][x + (dx1 || dx2)] === flower) + (grid[y + (dy1 || dy2)]?.[x] === flower);
-      console.log({dx1, dy1, dx2, dy2});
-      console.log({d1, d2});
-      if (d2 === 0 || (d1 === 1 && d2 === 2)) {
-        sum ++;
-      }
-    }
-  }
-  console.log({sum})
-  return sum;
-}
-
-function part24(input) {
-  const grid = input.split('\n').map(l => l.split(''));
-  const h = grid.length, w = grid[0].length;
-  const plots = {};
-  for (let x = 0; x < w; x++) for (let y = 0; y < h; y++) {
-    const flower = grid[y][x];
-    const plot = (plots[flower] ??= { l: 0, sides: 0 });
-    plot.l++;
-    for (let d = 0; d < DIRS.length; d++) {
-      const [dx1, dy1] = DIRS[d];
-      const [dx2, dy2] = DIRS[(d + 1) % 4];
-
-      const d1 = 1 + (grid[y + (dy1 || dy2)]?.[x + (dx1 || dx2)] === flower);
-      const d2 = (grid[y][x + (dx1 || dx2)] === flower) + (grid[y + (dy1 || dy2)]?.[x] === flower);
-      // console.log({dx1, dy1, dx2, dy2});
-      // console.log({d1, d2});
-      if (d2 === 0 || (d1 === 1 && d2 === 2)) {
-        plot.sides++;
-      }
-    }
-  }
-  return Object.values(plots).reduce((a, { l, sides }) => a + sides * l, 0);
-}
-
-// test(part2, `AAAA
-// BBCD
-// BBCC
-// EEEC`)
 
 const inputFile = 'inputs/2024/day-12.txt';
 const sampleInput = `
