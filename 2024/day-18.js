796ee3be590ec3dd368344cbe2c8cfd55c8cdbe3
Matt Dunlap <mpdunlap@gmail.com>
2024-12-18 11:33:03 -0700

    Day 18 optimization

--- 2024/day-18.js
+++ 2024/day-18.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env node
 import { exec, test } from '../helpers/exec.js';
 
-function part1(input, { size, bytes }) {
-  const blocks = new Set(input.split('\n').slice(0, bytes));
+function parseGrid(input, size, bytes) {
+  const grid = Array(size + 1).fill().map(() => Array(size + 1).fill(0));
+  grid.blocks = input.split('\n').slice(0, bytes).map(l => l.split(',').map(Number));
+  grid.blocks.forEach(([x, y]) => grid[y][x] = 1);
+  return grid;
+}
 
+function part1(input, { size, bytes }) {
+  const grid = Array.isArray(input) ? input : parseGrid(input, size, bytes);
   const wave = [[0,0,0]];
   for (let [x, y, d] of wave) {
     if (x === size && y === size) return d;
 
-    if (blocks.has(`${x},${y}`)) continue;
-    blocks.add(`${x},${y}`);
+    if (grid[y][x] === 1 || grid[y][x] === bytes) continue;
+    grid[y][x] = bytes;
 
     d += 1;
     if (x < size) wave.push([x+1, y, d]);
@@ -20,11 +26,12 @@ function part1(input, { size, bytes }) {
 }
 
 function part2(input, { size }) {
-  const lines = input.split('\n');
-  let bytes = lines.length;
-  while (bytes--)
-    if(part1(input, { size, bytes }))
-      return lines[bytes];
+  const grid = parseGrid(input, size, Infinity);
+  const blocks = grid.blocks;
+
+  for (let x, y; [x, y] = blocks.pop();)
+    if(grid[y][x] = 0, part1(grid, { size, bytes: blocks.length }))
+      return `${x},${y}`;
 }
 
 const inputFile = 'inputs/2024/day-18.txt';
@@ -59,4 +66,4 @@ test(part1, sampleInput, 22, { size: 6, bytes: 12 });
 exec(part1, inputFile, 310, { size: 70, bytes: 1024 });
 
 test(part2, sampleInput, '6,1', { size: 6 });
-exec(part2, inputFile, null, { size: 70 });
+exec(part2, inputFile, '16,46', { size: 70 });
