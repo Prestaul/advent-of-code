e0b7c49a06990c4d3f9c11dc3348c2d8fdbc8c4b
Matt Dunlap <mpdunlap@gmail.com>
2024-12-19 23:32:13 -0700

    Day 20 cleanup

--- 2024/day-20.js
+++ 2024/day-20.js
@@ -1,47 +1,30 @@
 #!/usr/bin/env node
 import { exec, test } from '../helpers/exec.js';
 
+const dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+
 function solve(input, time, limit = 100) {
   const grid = input.split('\n').map(l => l.split(''));
   const w = grid[0].length, h = grid.length;
   const dist = Array(h).fill().map(_ => Array(w).fill(Infinity));
+  const s = input.indexOf('S');
 
   // Get all walkable distances
-  const s = input.indexOf('S');
   const wave = [[s % (w + 1), Math.floor(s / (w + 1)), 0]];
-  for (const [x, y, d] of wave) {
-    if (grid[y][x] === '#' || dist[y][x] < Infinity) continue;
+  for (const [x, y, d] of wave) if (grid[y][x] !== '#' && dist[y][x] === Infinity) {
     dist[y][x] = d;
-
-    wave.push([x+1, y  , d+1]);
-    wave.push([x-1, y  , d+1]);
-    wave.push([x  , y+1, d+1]);
-    wave.push([x  , y-1, d+1]);
+    for (const [dx, dy] of dirs) wave.push([x + dx, y + dy, d + 1]);
   }
 
-  // Walk back from the end
-  const e = input.indexOf('E');
-  let x = e % (w + 1), y = Math.floor(e / (w + 1));
+  // Look at every cell and inspect the surrounding cells within `time` steps
   let count = 0;
-  while (dist[y][x] > 0) {
-    const d = dist[y][x];
-    const d2 = d - limit;
+  for (let x = 1; x < w - 1; x++) for(let y = 1; y < h - 1; y++) if (grid[y][x] !== '#') {
+    const d2 = dist[y][x] - limit;
 
-    // Look at all the spaces reachable during cheat time
-    for (let dx = -time; dx <= time; dx++) {
-      const ddx = Math.abs(dx);
-      for (let dy = ddx - time; dy <= time - ddx; dy++) {
+    for (let dx = -time, ddx = time; dx <= time; dx++, ddx = Math.abs(dx))
+      for (let dy = ddx - time; dy <= time - ddx; dy++)
         if ((dist[y + dy]?.[x + dx] ?? Infinity) <= (d2 - ddx - Math.abs(dy))) count += 1;
-      }
-    }
-
-    // Move to the next space
-    if (dist[y - 1][x] < d) y -= 1;
-    else if (dist[y + 1][x] < d) y += 1;
-    else if (dist[y][x - 1] < d) x -= 1;
-    else x += 1;
   }
-
   return count;
 }
 
